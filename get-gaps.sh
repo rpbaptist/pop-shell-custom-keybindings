d54e1ce895e0d4c5a107efc66ec97d1f368ed767
Richard Baptist <richard@remote.com>
2024-05-24 11:01:25 +0200

    Use mine

--- get-gaps.sh
+++ get-gaps.sh
@@ -9,15 +9,11 @@
 ##############################################################################
 
 # Define the dconf branches
-KEYS_GNOME_WM=/org/gnome/desktop/wm/keybindings
-KEYS_GNOME_SHELL=/org/gnome/shell/keybindings
-KEYS_MUTTER=/org/gnome/mutter/keybindings
-KEYS_MEDIA=/org/gnome/settings-daemon/plugins/media-keys
 KEYS_POP=/org/gnome/shell/extensions/pop-shell
 
-uint32_t g = $(dconf read ${KEYS_POP}/gap-inner) # | cut -c8)
-echo "${g}"
-g=$((g+1))
-echo "${g}"
-dconf write ${KEYS_POP}/gap-inner "${g}"
-dconf write ${KEYS_POP}/gap-outer "${g}"
+# uint32_t g = $(dconf read ${KEYS_POP}/gap-inner) # | cut -c8)
+# echo "${g}"
+# g=$((g+1))
+# echo "${g}"
+dconf write ${KEYS_POP}/gap-inner 3
+dconf write ${KEYS_POP}/gap-outer 2
